4d7696f575276a3c1c86fbc6427346eba5b3d35a
Darren Neimke <darren.neimke@gmail.com>
2021-02-06 15:33:18 +1030

    Added EditForm with custom style provider

---
+++ Core/BootstrapStyleProvider.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+using System.Linq;
+
+namespace Blazor.Samples.Core
+{
+    public class BootstrapStyleProvider : FieldCssClassProvider
+    {
+        public override string GetFieldCssClass(EditContext editContext, in FieldIdentifier fieldIdentifier)
+        {
+            var isValid = !editContext.GetValidationMessages(fieldIdentifier).Any();
+
+            return isValid ? "is-valid was-validated" : "is-invalid was-validated";
+        }
+    }
+}
---
+++ Models/ContactDetails.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Blazor.Samples.Models
+{
+    public class ContactDetails
+    {
+        [Required]
+        [StringLength(25, ErrorMessage = "First Name is too long.")]
+        public string FirstName { get; set; }
+
+
+        public bool IsOwner { get; set; }
+
+        [StringLength(500, ErrorMessage = "Maximum length for comments is 500 characters.")]
+        public string Comments { get; set; }
+    }
+}
